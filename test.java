6e3855975541e6520a6ebff75955f8475cdbcb8a
qqlssid <111620807+qqlssid@users.noreply.github.com>
2022-08-20 11:00:22 +0900

    Create test.java

---
+++ test.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class asd {
+		public static void main(String[] args) {
+			   
+			 int kor;
+			 int mat;
+			 int eng;
+			 int sum;
+
+			 kor = 90;
+			 mat = 80;
+			 eng= 100;
+
+			 sum = kor + mat + eng;
+
+			  System.out.println(kor+"+"+mat+"+"+eng+"="+sum);
+
+	}
+
+}
