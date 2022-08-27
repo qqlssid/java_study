3cac7b0c9a006c0ddcbb64fa524b2caf796eea9f
qqlssid <111620807+qqlssid@users.noreply.github.com>
2022-08-27 10:40:55 +0900

    Create java_study10.java

---
+++ java_study10.java
@@ -0,0 +1,12 @@
+package hello;
+import java.util.*;
+public class javanew {
+	public static void main(String[] args) {
+		int x;
+		Scanner in = new Scanner(System.in);
+		x = in.nextInt();
+		System.out.print((x++)+"\n");     <<다음 줄로 이동"\n" n is next line
+		System.out.print((++x));
+	}
+
+}
