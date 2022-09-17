50c6b9edf0ace1a815b54aa9944877fd6e006b91
qqlssid <111620807+qqlssid@users.noreply.github.com>
2022-09-17 14:14:32 +0900

    Create 28.java

---
+++ 28.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+	    int i = 1;
+	    while (i<16) {
+	    	System.out.print(i+" ");
+	    	i++;
+	    	
+	    	
+	    }
+	}
+}
+//1부터 15까지 차례로 출력하는 프로그램을 작성하시오. while문을 이용하세요.
