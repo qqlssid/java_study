d0365b7bb18a14b5c21bb135ce631eab12520cb3
qqlssid <111620807+qqlssid@users.noreply.github.com>
2022-10-01 15:08:13 +0900

    Create 34.java

---
+++ 34.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		int i=1;
+		int x;
+		
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		
+		while(i <= x) {
+			System.out.print(i+" ");
+			i++;
+		}
+	}
+}	
+//정수를 입력받아 1부터 입력받은 정수까지를 차례대로 출력하는 프로그램을 작성하시오.
