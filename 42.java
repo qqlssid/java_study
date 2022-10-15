488c01089d3a91c408eaba8ba551a59fea7debe9
qqlssid <111620807+qqlssid@users.noreply.github.com>
2022-10-15 12:48:35 +0900

    Create 42.java

---
+++ 42.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		
+		for(int i=0; i<5; i++) {
+			for(int j = 2; j < 7; j++)  //j를 2를 기준으로 하여 증가시킨다
+				
+				
+				
+				System.out.print(j+i+" ");  //j라인이 끝날 때마다 i라는 변수가 1씩 증가한다.
+		 
+			System.out.println();
+		}
+	}
+}
